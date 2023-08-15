274de11783f3c0f9a6f56b2819642f7bbc850bcf
Chris Rico <chris.r.rico@gmail.com>
2023-08-14 17:07:45 -0700

    added variables to get element by id's. Added event listener to see if boxes are checked before generating password. Created function to genrate a function

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,3 +1,31 @@
+const resultEL = document.getElementById('result');
+const lengthEL = document.getElementById('length');
+const uppercaseEL = document.getElementById('uppercase');
+const lowercaseEL = document.getElementById('lowercase');
+const numbersEL = document.getElementById('numbers');
+const symbolsEL = document.getElementById('symbols');
+const generateEL = document.getElementById('generate');
+const clipboardEL = document.getElementById('clipboard');
+
+
+generateEL.addEventListener('click', () => {
+    const length = +lengthEL.value;
+    const hasLower = lowercaseEL.checked;
+    const hasUpper = uppercaseEL.checked;
+    const hasNumber = numbersEL.checked;
+    const hasSymbol = symbolsEL.checked;
+
+    resultEL.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+    console.log(typesArr)
+}
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
@@ -22,6 +50,3 @@ function getRandomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
-
-
-console.log(getRandomSymbol())
