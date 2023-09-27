e5e085935a4861d05fa69b2be0c2a958a8979b29
Chris Rico <chris.r.rico@gmail.com>
2023-09-26 23:05:44 -0700

    added variables of get element by ID in the html. Added eventlistener to sharpen the image by the filter blur effect by shorting the blur value.

--- Password strength/script.js
+++ Password strength/script.js
@@ -3,6 +3,8 @@ const background = document.getElementById('background');
 
 password.addEventListener('input', (e) => {
     const input = e.target.value
-
-    console.log(input)
+    const length = input.length
+    const blurValue = 20 - length * 2
+    background.style.filter = `blur(${blurValue}px)`
+    
 })
