6549e78b1d03ace1f18c216e55f6b1adcbc4fb5b
Chris Rico <chris.r.rico@gmail.com>
2023-10-16 15:07:51 -0700

    added styling to ratings container and images. And added style to button. Also created variables to target the classes in hmtl. Added a event listener that is activated when clicked on image or rating

---
+++ Feedback UI Design/script.js
@@ -0,0 +1,9 @@
+const rating = document.querySelectorAll('.rating');
+const sendBtn = document.querySelector('#send');
+const panel = document.querySelector('#panel');
+
+panel.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        e.target.parentNode.classList.add('active')
+    }
+})
