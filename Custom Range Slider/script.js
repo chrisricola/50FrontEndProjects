e1c7a4f275f2771fd5c59d0a9471637d7226e258
Chris Rico <chris.r.rico@gmail.com>
2023-10-24 21:40:58 -0700

    added a variable from stack overflow to map a range of numbers to another range of numbers to have the lbale and range in the exact px's when sliding

--- Custom Range Slider/script.js
+++ Custom Range Slider/script.js
@@ -13,9 +13,14 @@ range.addEventListener('input', (e) => {
     const max = +e.target.max
     const min = +e.target.min
 
-    const left = value * (num_width /  max) - num_label_width / 2
+    const left = value * (num_width /  max) - num_label_width / 2 + scale(value, min, max, 10, -10)
 
     label.style.left = `${left}px`
 
     label.innerHTML = value
 })
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
