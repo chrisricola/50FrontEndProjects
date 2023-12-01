4e033019dd8a1d10fdb89984fb7ff7c24ed2a15e
Chris Rico <chris.r.rico@gmail.com>
2023-11-30 22:15:59 -0800

    added variable to add local storage. Added function to update local storage.

--- Todo list/script.js
+++ Todo list/script.js
@@ -1,6 +1,12 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-const todosUL = document.getElementById('todos');
+const todosUl = document.getElementById('todos');
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -17,23 +23,40 @@ function addTodo(todo) {
     }
     
     if(todoText) {
-        const todoEL = document.createElement('li')
+        const todoEl = document.createElement('li')
         if(todo && todo.completed) {
-            todoEL.classList.add('conpleted')
+            todoEl.classList.add('conpleted')
         }
 
-        todoEL.innerText = todoText
+        todoEl.innerText = todoText
 
-        todoEL.addEventListener('click', () => todoEL.classList.toggle('completed'))
+        todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'))
 
-        todoEL.addEventListener('contextmenu', (e) => {
+        todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault()
 
-            todoEL.remove()
+            todoEl.remove()
         })
 
-        todosUL.appendChild(todoEL)
+        todosUl.appendChild(todoEl)
 
-        input.value = ''
+        input.value = '';
+
+        updateLS()
     }
+}
+
+function updateLS() {
+    todosEl = document.querySelectorAll('li')
+
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
