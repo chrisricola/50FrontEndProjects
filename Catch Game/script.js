1578eed15434c3691e5168cae57ed7184f4a8dd8
Chris Rico <chris.r.rico@gmail.com>
2023-12-18 22:48:05 -0800

    added a for each loop for when the eventlistener of the insect img is click to grab the image, source and alt to then add the image to the following page in a random spot. Added a function to create the insect that creates a div of the image in a random spot. Added a function to create a random location on the window of the DOM

--- Catch Game/script.js
+++ Catch Game/script.js
@@ -10,3 +10,34 @@ let score = 0;
 let selected_insect = {}
 
 start_btn.addEventListener('click', () => screens[0].classList.add('up'))
+
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img')
+        const src = img.getAttribute('src')
+        const alt = img.getAttribute('alt')
+        selected_insect = {src, alt}
+        screens[1].classList.add('up')
+        setTimeout(createInsect, 1000)
+        startGame()
+    })
+})
+
+function createInsect() {
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+    const { x, y } = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src ="${selected_insect.src}" style="transform: rotate(${Math.random() * 360}deg)" />`
+
+    game_container.appendChild(insect)
+}
+
+function getRandomLocation() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const x = Math.random() * (width - 200) + 100
+    const y = Math.random() * (height - 200) + 100
+    return { x, y }
+}
